85f946a4f717c5802250218ba2931afd8b43ca96
YLX <568414379@qq.com>
2017-09-07 16:24:45 +0800

    建立工程文件

---
+++ src/main/java/com/ylx/librarymanage/LibrarymanageApplication.java
@@ -0,0 +1,12 @@
+package com.ylx.librarymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanageApplication.class, args);
+	}
+}
